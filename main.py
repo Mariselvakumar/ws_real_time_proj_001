aaa51a50890413fe6247e9085e0c786ad629526d
JohnsonJDoss <145665449+JohnsonJDoss@users.noreply.github.com>
2023-09-26 15:26:05 +0530

    first_code_setup

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello World')
+inp_str = 'bbbwwweeerrrtttyy'
+out_str, count, target_char = '', 0, inp_str[0]
+
+for x in inp_str:
+    if x == target_char:
+        count += 1
+        continue
+    else:
+        out_str = out_str + target_char + str(count)
+        count, target_char = 1, x
+        continue
+print(out_str)
+
